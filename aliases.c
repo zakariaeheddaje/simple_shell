7aeab09fc4dc7dcad9836e4c0bf040c6ea81967a
hajarelhassnaoui <elhassnaouihajarr@gmail.com>
2023-09-20 15:23:21 -0700

    hi

--- aliases.c
+++ aliases.c
@@ -1,142 +1,152 @@
 #include "shell.h"
 
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias(char *var_name, char *value);
+void print_alias(alias_t *alias);
+
 /**
- * unsetAlias - Removes an alias.
+ * shellby_alias - Builtin command that either prints all aliases, specific
+ * aliases, or sets an alias.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- * @info: Pointer to the parameter structure.
- * @str: String representing the alias to be removed.
- *
- * Return: 0 if the alias was successfully unset, 1 if there was an error.
-*/
-
-int unsetAlias(info_t *info, char *str)
+ * Return: If an error occurs - -1.
+ *         Otherwise - 0.
+ */
+int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 {
-	char *p, c;
-	int ret;
+	alias_t *temp = aliases;
+	int i, ret = 0;
+	char *value;
 
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	c = *p;
-	*p = 0;
-	ret = deleteNode(&(info->alias),
-		getNodeIndex(info->alias, nodeStartsWith(info->alias, str, -1)));
-	*p = c;
+	if (!args[0])
+	{
+		while (temp)
+		{
+			print_alias(temp);
+			temp = temp->next;
+		}
+		return (ret);
+	}
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		value = _strchr(args[i], '=');
+		if (!value)
+		{
+			while (temp)
+			{
+				if (_strcmp(args[i], temp->name) == 0)
+				{
+					print_alias(temp);
+					break;
+				}
+				temp = temp->next;
+			}
+			if (!temp)
+				ret = create_error(args + i, 1);
+		}
+		else
+			set_alias(args[i], value);
+	}
 	return (ret);
 }
 
 /**
- * setAlias - Creates or modifies an alias.
- *
- * @info: Pointer to the parameter structure.
- * @str: String representing the alias to be set.
- *
- * Return: 0 if the alias was successfully set, 1 if there was an error.
-*/
-
-
-int setAlias(info_t *info, char *str)
-{
-	char *p;
-
-	p = _strchr(str, '=');
-	if (!p)
-		return (1);
-	if (!*++p)
-		return (unsetAlias(info, str));
-
-	unsetAlias(info, str);
-	return (endNode(&(info->alias), str, 0) == NULL);
-}
-
-/**
- * printAlias - Displays an alias.
- *
- * @node: Alias node to be printed.
- *
- * Return: 0 if the alias was successfully printed, 1 if there was an error.
+ * set_alias - Will either set an existing alias 'name' with a new value,
+ * 'value' or creates a new alias with 'name' and 'value'.
+ * @var_name: Name of the alias.
+ * @value: Value of the alias. First character is a '='.
  */
-
-int printAlias(list_t *node)
+void set_alias(char *var_name, char *value)
 {
-	char *p = NULL, *a = NULL;
+	alias_t *temp = aliases;
+	int len, j, k;
+	char *new_value;
 
-	if (node)
+	*value = '\0';
+	value++;
+	len = _strlen(value) - _strspn(value, "'\"");
+	new_value = malloc(sizeof(char) * (len + 1));
+	if (!new_value)
+		return;
+	for (j = 0, k = 0; value[j]; j++)
 	{
-		p = _strchr(node->str, '=');
-		for (a = node->str; a <= p; a++)
-			_putchar(*a);
-		_putchar('\'');
-		_puts(p + 1);
-		_puts("'\n");
-		return (0);
+		if (value[j] != '\'' && value[j] != '"')
+			new_value[k++] = value[j];
 	}
-	return (1);
-}
-
-/**
- * myAlias - Simulates the "alias" built-in command (man alias).
- *
- * @info: Structure containing potential arguments.
- *
- * Return: Always returns 0.
- */
-
-int myAlias(info_t *info)
-{
-	int i = 0;
-	char *p = NULL;
-	list_t *node = NULL;
-
-	if (info->argc == 1)
+	new_value[k] = '\0';
+	while (temp)
 	{
-		node = info->alias;
-		while (node)
+		if (_strcmp(var_name, temp->name) == 0)
 		{
-			printAlias(node);
-			node = node->next;
+			free(temp->value);
+			temp->value = new_value;
+			break;
 		}
-		return (0);
+		temp = temp->next;
 	}
-	for (i = 1; info->argv[i]; i++)
-	{
-		p = _strchr(info->argv[i], '=');
-		if (p)
-			setAlias(info, info->argv[i]);
-		else
-			printAlias(nodeStartsWith(info->alias, info->argv[i], '='));
-	}
-
-	return (0);
+	if (!temp)
+		add_alias_end(&aliases, var_name, new_value);
 }
 
 /**
- * replaceAlias - Replaces aliases in the tokenized string.
- *
- * @info: The parameter structure.
- *
- * Return: 1 if any aliases were replaced, 0 otherwise.
+ * print_alias - Prints the alias in the format name='value'.
+ * @alias: Pointer to an alias.
  */
-
-int replaceAlias(info_t *info)
+void print_alias(alias_t *alias)
 {
-	int i;
-	list_t *node;
-	char *p;
+	char *alias_string;
+	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
 
-	for (i = 0; i < 10; i++)
+	alias_string = malloc(sizeof(char) * (len + 1));
+	if (!alias_string)
+		return;
+	_strcpy(alias_string, alias->name);
+	_strcat(alias_string, "='");
+	_strcat(alias_string, alias->value);
+	_strcat(alias_string, "'\n");
+
+	write(STDOUT_FILENO, alias_string, len);
+	free(alias_string);
+}
+/**
+ * replace_aliases - Goes through the arguments and replace any matching alias
+ * with their value.
+ * @args: 2D pointer to the arguments.
+ *
+ * Return: 2D pointer to the arguments.
+ */
+char **replace_aliases(char **args)
+{
+	alias_t *temp;
+	int i;
+	char *new_value;
+
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (i = 0; args[i]; i++)
 	{
-		node = nodeStartsWith(info->alias, info->argv[0], '=');
-		if (!node)
-			return (0);
-		free(info->argv[0]);
-		p = _strchr(node->str, '=');
-		if (!p)
-			return (0);
-		p = _strDuplicate(p + 1);
-		if (!p)
-			return (0);
-		info->argv[0] = p;
+		temp = aliases;
+		while (temp)
+		{
+			if (_strcmp(args[i], temp->name) == 0)
+			{
+				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
+				if (!new_value)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_value, temp->value);
+				free(args[i]);
+				args[i] = new_value;
+				i--;
+				break;
+			}
+			temp = temp->next;
+		}
 	}
-	return (1);
+
+	return (args);
 }
